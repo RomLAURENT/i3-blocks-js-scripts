8448483bd98e836c981ac3f6cdd6ae793b57d3ab
tadouane <tadouane@express-mailing.com>
2020-05-28 17:59:13 +0200

    Mise a jour du module

--- battery.js
+++ battery.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-const templates = require("./conf.json");
-const { execAsync, update, jobPlanner, eventHandler } = require("./blocks");
+const {templates} = require("./conf.json");
+const { formatText, execAsync, update, jobPlanner, eventHandler } = require("./i3-blocks-helper");
 
 async function getRemaining() {
     const [, , remaining_res] = await execAsync(`cat /sys/class/power_supply/BAT1/capacity`);
@@ -13,15 +13,58 @@ async function isCharging() {
     return status_res.trim() === 'Charging';
 }
 
-const mainJob = jobPlanner(async ({ symbol_charging, symbol_discharging }) => {
-    const remaining_value = await getRemaining();
+const mainJob = jobPlanner(async ({
+    charging_frmt=`B: %remaining%%+`,
+    discharging_frmt=`B: %remaining%%-`,
+    template=`normal`,
+    discharged_template=`alert`,
+    discharged_trigger=30,
+}) => {
+    const remaining = await getRemaining();
     const is_charging = await isCharging();
-    const symbol = is_charging ? symbol_charging : symbol_discharging;
-    const remaining_label = ` <span size='small'>${remaining_value}%</span>`
+
+    //const symbol = is_charging ? symbol_charging : symbol_discharging;
+    //const remaining_label = ` <span size='small'>${remaining_value}%</span>`
     update(
-        templates.normal,
-        {
-            full_text: `${symbol}${remaining_label}`,
-        }
+        templates[remaining < discharged_trigger ? discharged_template : template],
+        formatText(is_charging ? charging_frmt : discharging_frmt, {remaining}),
     );
 }, 5000);
+
+eventHandler(async ({ button, left_click, wheel_click, right_click, wheel_up, wheel_down }) => {
+    switch (button) {
+        case 1:
+            {
+                if (left_click) await execAsync(left_click);
+            }
+            break;
+
+        case 2:
+            {
+                if (wheel_click) await execAsync(wheel_click);
+            }
+            break;
+
+        case 3:
+            {
+                if (right_click) await execAsync(right_click);
+            }
+            break;
+
+        case 4:
+            {
+                if (wheel_up) await execAsync(wheel_up);
+            }
+            break;
+
+        case 5:
+            {
+                if (wheel_down) await execAsync(wheel_down);
+            }
+            break;
+    }
+});
+
+process.on('SIGUSR1', () => {
+    mainJob.resume(true);
+});
