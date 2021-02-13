5838f5261278ddc8977295c34c983d3c6c9e7ffc
Romain LAURENT <rlaurent@hotmail.fr>
2021-02-13 05:11:57 +0100

    simplifixation du switch de device/port du block pa

--- pa.js
+++ pa.js
@@ -58,42 +58,71 @@ eventHandler(async ({ mode, button }) => {
                 }
                 break;
 
-            case 2:
+            case 3:
                 {
                     const status = await getPAStatus(mode);
-                    const default_entry = status
-                        .filter(c => c.default)
-                        .map(d => d.value)[0];
+
                     const entries = status
-                        .filter(c => c.default || c.properties["device.class"] != "monitor")
-                        .map(d => d.value);
-                    const streams = (await getStreams(mode, default_entry))
-                        .map(d => d.value);
+                        .filter(d => d.default || d.properties["device.class"] != "monitor")
+                        .flatMap(d => Object.keys(d.ports).map(p => ({
+                            device: d.value,
+                            port: p,
+                            current: !!(d.default && p === d["active port"]),
+                        })));
 
-                    const new_default = entries[(entries.indexOf(default_entry) + 1) % entries.length];
+                    const current_entry = entries.filter(c => c.current)[0];
 
-                    await execAsync(`pactl set-default-${mode} ${new_default}`);
-                    for (const stream of streams) {
-                        await execAsync(`pacmd move-${mode}-input ${stream} ${new_default}`);
+                    const new_default = entries[(entries.indexOf(current_entry) + 1) % entries.length];
+
+                    await execAsync(`pactl set-${mode}-port ${new_default.device} ${new_default.port}`);
+
+                    if(current_entry.device !== new_default.device) {
+                        await execAsync(`pactl set-default-${mode} ${new_default.device}`);
+                        const streams = (await getStreams(mode, current_entry.device)).map(d => d.value);
+
+                        for (const stream of streams) {
+                            await execAsync(`pacmd move-${mode}-input ${stream} ${new_default.device}`);
+                        }
                     }
                     mainJob.resume(true);
                 }
-                break;
+            //     {
+            //         const status = await getPAStatus(mode);
 
-            case 3:
-                {
-                    const all_entries = await getPAStatus(mode);
-                    const default_entry = all_entries.filter(c => c.default)[0];
-                    const active_port = default_entry["active port"];
-                    const ports = Object.keys(default_entry["ports"]);
+            //         const default_entry = status
+            //             .filter(c => c.default)
+            //             .map(d => d.value)[0];
 
-                    const new_port = ports[(ports.indexOf(active_port) + 1) % ports.length];
+            //         const entries = status
+            //             .filter(c => c.default || c.properties["device.class"] != "monitor")
+            //             .map(d => d.value);
 
-                    await execAsync(`pactl set-${mode}-port ${default_dev} ${new_port}`);
-                    mainJob.resume(true);
-                }
-                break;
+            //         const streams = (await getStreams(mode, default_entry))
+            //             .map(d => d.value);
 
+            //         const new_default = entries[(entries.indexOf(default_entry) + 1) % entries.length];
+
+            //         await execAsync(`pactl set-default-${mode} ${new_default}`);
+            //         for (const stream of streams) {
+            //             await execAsync(`pacmd move-${mode}-input ${stream} ${new_default}`);
+            //         }
+            //         mainJob.resume(true);
+            //     }
+            //     break;
+
+            // case 2:
+            //     {
+            //         const all_entries = await getPAStatus(mode);
+            //         const default_entry = all_entries.filter(c => c.default)[0];
+            //         const active_port = default_entry["active port"];
+            //         const ports = Object.keys(default_entry["ports"]);
+
+            //         const new_port = ports[(ports.indexOf(active_port) + 1) % ports.length];
+
+            //         await execAsync(`pactl set-${mode}-port ${default_dev} ${new_port}`);
+            //         mainJob.resume(true);
+            //     }
+            //     break;
             case 4:
                 {
                     await execAsync(`pactl set-${mode}-volume ${default_dev} +5%`);
@@ -115,4 +144,4 @@ eventHandler(async ({ mode, button }) => {
 
 process.on('SIGUSR1', () => {
     mainJob.resume(true);
-})
+});
